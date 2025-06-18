e049e0b12495a0781734b49620f141d206a32aef
Nataliia Zhelizniakova <nataliia.zhelizniakova@avocado.com.au>
2025-06-18 18:25:14 +1000

    added more steps

--- src/step-definitions/Homepage_Steps.ts
+++ src/step-definitions/Homepage_Steps.ts
@@ -1,4 +1,4 @@
-import { Given, When } from "@cucumber/cucumber";
+import { Given, When, Then } from "@cucumber/cucumber";
 import { chromium, Browser, Page } from "playwright"
 
 let browser: Browser; //Represents the browser instance (e.g., Chrome, Firefox) opened by Playwright.
@@ -7,10 +7,10 @@ let page: Page; //Represents a single web page within a context
 
 const url = "https://www.webdriveruniversity.com/";
 
-Given('homepage', async () => {
+Given('I navigated to homepage', async () => {
     //Setup browser instance:
     browser = await chromium.launch({ headless: false });
-    context = await browser.newContext({ viewport: { width: 1920, height: 1080 }});
+    context = await browser.newContext({ viewport: { width: 1920, height: 1080 } });
     page = await context.newPage();
 
     //Access URL
@@ -21,19 +21,4 @@ When('I click on the contact us button', async () => {
     //await page.pause();
     const contactUs_Button = await page.getByRole('link', { name: 'CONTACT US Contact Us Form' });
     await contactUs_Button.click();
-});
-
-When('I switch to the new browser tab', async () => {
-    // Wait for the new tab to open
-    page = context.waitForEvent('page');
-
-    // Switch to the new page
-    await page.bringToFront();
-    
-});
-
-When('I type a first name', async () => {
-    const firstName_Input = await page.getByPlaceholder('First Name');
-    await firstName_Input.fill('John');
-    
 });
