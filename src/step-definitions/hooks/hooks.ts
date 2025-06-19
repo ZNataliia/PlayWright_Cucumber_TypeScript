33b603ba09ade247cca21897f80cec5d94a46c9a
Nataliia Zhelizniakova <nataliia.zhelizniakova@avocado.com.au>
2025-06-19 15:40:49 +1000

    adding screenshots to reports for failed tests

--- src/step-definitions/hooks/hooks.ts
+++ src/step-definitions/hooks/hooks.ts
@@ -1,4 +1,4 @@
-import { After, AfterAll, Before, BeforeAll } from "@cucumber/cucumber";
+import { After, AfterAll, Before, BeforeAll, Status } from "@cucumber/cucumber";
 import {Browser, chromium} from "@playwright/test";
 import { pageFixture } from "./browserContextFixture";
 
@@ -17,12 +17,25 @@ AfterAll(async function(){
 // Before hook: Runs before each scenario
 Before(async function() {
     browser = await chromium.launch({ headless: false });
-    pageFixture.context = await browser.newContext({ viewport: { width: 1680, height: 1050 } });
+    pageFixture.context = await browser.newContext({ viewport: { width: 1920, height: 1080 } });
     pageFixture.page = await pageFixture.context.newPage();
 })
 
 // After hook: Runs after each scenario
-After(async function() {
+After(async function({pickle, result}) {
+    if(result?.status === Status.FAILED) {
+        if(pageFixture.page) {
+            const screenshotPath = `./reports/screenshots/${pickle.name}-${Date.now()}.png`;
+            const image = await pageFixture.page.screenshot({
+                path: screenshotPath,
+                type: 'png',
+                //timeout: 60000
+            });
+            await this.attach(image, 'image/png');
+        } else {
+            console.error('pageFixture.page is undefined');
+        }
+    }
     await pageFixture.page.close();
     await browser.close();
 })
